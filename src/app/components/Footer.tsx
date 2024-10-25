0f9fa43efdf82182771dd56204c898edf0473ad2
Denis Podanenko <podanenko826@icloud.com>
2024-10-25 16:14:52 +0300

    Make footer responsive for mobile

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -55,79 +55,83 @@ const Footer = () => {
             <div
                 className={`${styles.footer} container-fluid justify-content-center`}>
                 <div className="container d-flex">
-                    <div className="col-7 pt-3">
-                        <h1 className="heading-large" id={styles.subscribeText}>
-                            Stay informed, <br />
-                            subscribe to Yalovets Blog!
-                        </h1>
-                        <p className={`${styles.navText} pt-2`}>
-                            Keep in touch with latest information in tech
-                            industry
-                        </p>
-                    </div>
-                    <div className="d-flex">
-                        <form className="row p-3">
-                            <input
-                                type="text"
-                                name="firstName"
-                                placeholder="First name"
-                                value={subscribeData?.firstName}
-                                onChange={e =>
-                                    handleSubscribeDataChange(
-                                        e.target.value,
-                                        e.target.name
-                                    )
-                                }
-                                id={styles.inputBox}
-                            />
-                            <input
-                                type="email"
-                                name="email"
-                                placeholder="Email address"
-                                value={subscribeData?.email}
-                                onChange={e =>
-                                    handleSubscribeDataChange(
-                                        e.target.value,
-                                        e.target.name
-                                    )
-                                }
-                                id={styles.inputBox}
-                            />
-                            <div className="d-flex align-content-start">
+                    <div className="row">
+                        <div className="col-md-7 pt-3">
+                            <h1
+                                className="heading-large"
+                                id={styles.subscribeText}>
+                                Stay informed, <br />
+                                subscribe to Yalovets Blog!
+                            </h1>
+                            <p className={`${styles.navText} pt-2`}>
+                                Keep in touch with latest information in tech
+                                industry
+                            </p>
+                        </div>
+                        <div className="d-flex col-md-5">
+                            <form className="row" id={styles.subscribeForm}>
                                 <input
-                                    type="checkbox"
-                                    checked={agreedToTerms}
-                                    onChange={(e: any) =>
-                                        handleCheckboxChange(e)
+                                    type="text"
+                                    name="firstName"
+                                    placeholder="First name"
+                                    value={subscribeData?.firstName}
+                                    onChange={e =>
+                                        handleSubscribeDataChange(
+                                            e.target.value,
+                                            e.target.name
+                                        )
                                     }
-                                    id={styles.agreementCheckbox}
+                                    id={styles.inputBox}
                                 />
-                                <p className={styles.navText}>
-                                    I want to subscribe to the newsletter with
-                                    new content and recieve annoucements
-                                    regarding new products and services. I agree
-                                    with the{' '}
-                                    <a href="#" className={styles.navLink}>
-                                        Privacy Policy.
-                                    </a>
-                                </p>
-                            </div>
-                            <button
-                                type="submit"
-                                disabled={
-                                    !agreedToTerms ||
-                                    subscribeData.firstName === '' ||
-                                    subscribeData.email === ''
-                                }
-                                className={styles.btn_subscribe}>
-                                Subscribe
-                            </button>
-                        </form>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="Email address"
+                                    value={subscribeData?.email}
+                                    onChange={e =>
+                                        handleSubscribeDataChange(
+                                            e.target.value,
+                                            e.target.name
+                                        )
+                                    }
+                                    id={styles.inputBox}
+                                />
+                                <div className="d-flex align-content-start">
+                                    <input
+                                        type="checkbox"
+                                        checked={agreedToTerms}
+                                        onChange={(e: any) =>
+                                            handleCheckboxChange(e)
+                                        }
+                                        id={styles.agreementCheckbox}
+                                    />
+                                    <p className={styles.navText}>
+                                        I want to subscribe to the newsletter
+                                        with new content and recieve
+                                        annoucements regarding new products and
+                                        services. I agree with the{' '}
+                                        <a href="#" className={styles.navLink}>
+                                            Privacy Policy.
+                                        </a>
+                                    </p>
+                                </div>
+                                <button
+                                    type="submit"
+                                    disabled={
+                                        !agreedToTerms ||
+                                        subscribeData.firstName === '' ||
+                                        subscribeData.email === ''
+                                    }
+                                    className={styles.btn_subscribe}>
+                                    Subscribe
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
                 <div className="container justify-content-between py-4">
                     <div className="row justify-content-between">
-                        <div className="col-md-3">
+                        <div className="col-md-3 pt-">
                             <h4 className={styles.navText}>Links</h4>
                             <div className="horisontal-line horisontal-line-thin" />
                             <ul className="row gap-3">
@@ -143,7 +147,7 @@ const Footer = () => {
                                 ))}
                             </ul>
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-3 pt-4">
                             <h4 className={styles.navText}>Legal</h4>
                             <div className="horisontal-line horisontal-line-thin" />
                             <ul className="row justify-content-end gap-3">
