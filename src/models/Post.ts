224b541e47dd3e3441be95dc3e88fadb22b5d464
Denis Podanenko <podanenko826@icloud.com>
2024-11-03 14:40:23 +0200

    Update Post model

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,19 +1,13 @@
-import mongoose from 'mongoose';
-
-const postSchema = new mongoose.Schema({
+const Post = {
     slug: {type: String, reguired: true, unique: true},
+    email: {type: String, reguired: true, unique: true},
     title: {type: String, reguired: true, unique: false},
-    description: {type: String, reguired: false, unique: false},
+    description: {type: String, reguired: true, unique: false},
     imageUrl: {type: String, reguired: false, unique: true},
     date: {type: String, required: true, unique: false},
-    authorEmail: {type: String, reguired: true, unique: true},
+    modifyDate: {type: String, required: true, unique: false},
     readTime: {type: Number, reguired: true, unique: false},
     viewsCount: {type: Number, reguired: true, unique: false},
-});
+};
 
-if (process.env.NODE_ENV === 'production') {
-    postSchema.set('autoIndex', false);
-}
-
-const Post = mongoose.models.Post || mongoose.model('Post', postSchema);
 export default Post;
