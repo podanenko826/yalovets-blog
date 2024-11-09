64f1320fac8724e82548861ce732ce752c556d08
Denis Podanenko <podanenko826@icloud.com>
2024-11-09 15:49:07 +0200

    Update getUsers function

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,19 +1,23 @@
 import {NextResponse} from 'next/server';
 
-export const getUsers = async (email: string) => {
+export const getUsers = async (email?: string) => {
     const baseUrl =
         typeof window === 'undefined'
             ? process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000'
             : '';
 
     try {
-        const response = await fetch(`${baseUrl}/api/db?email=${email}`);
+        if (email) {
+            const response = await fetch(`${baseUrl}/api/user?email=${email}`);
+            const data = await response.json();
 
-        const data = await response.json();
+            return NextResponse.json(data, {status: 201});
+        } else {
+            const response = await fetch(`${baseUrl}/api/user`);
+            const data = await response.json();
 
-        console.log(data);
-
-        return NextResponse.json(data, {status: 201});
+            return NextResponse.json(data, {status: 201});
+        }
     } catch (err) {
         console.error('Failed to fetch users from the database.');
         return NextResponse.json(err, {status: 500});
