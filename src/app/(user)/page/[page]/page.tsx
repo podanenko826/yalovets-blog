a9fa1d096954376f220d65f1e8eb8f50396503cc
Denis Podanenko <podanenko826@icloud.com>
2024-11-13 15:48:45 +0200

    Bring back Blog page

--- src/app/(user)/page/[page]/page.tsx
+++ src/app/(user)/page/[page]/page.tsx
@@ -1,60 +1,66 @@
-// import React from 'react';
-// import PostCard from '@/components/PostCard';
+import React from 'react';
+import PostCard from '@/components/PostCard';
 
-// const ARTICLES_PER_PAGE = 10;
+import {getSortedPosts} from '@/lib/posts';
+import {getUsers} from '@/lib/users';
+import {AuthorItem, PostItem} from '@/types';
 
-// export async function generateStaticParams() {
-//     const articles = getSortedArticles();
-//     const pageCount = Math.ceil(articles.length / ARTICLES_PER_PAGE);
+const ARTICLES_PER_PAGE = 10;
 
-//     return Array.from({length: pageCount}, (_, i) => ({
-//         page: (i + 1).toString(),
-//     }));
-// }
+export async function generateStaticParams() {
+    const posts = await getSortedPosts();
+    const pageCount = Math.ceil(posts.length / ARTICLES_PER_PAGE);
 
-// export default async function BlogPage({params}: {params: {page: string}}) {
-//     const currentPage = parseInt(params.page, 10) || 1;
-//     const articles = getSortedArticles();
+    return Array.from({length: pageCount}, (_, i) => ({
+        page: (i + 1).toString(),
+    }));
+}
 
-//     const startIndex = (currentPage - 1) * ARTICLES_PER_PAGE;
-//     const paginatedArticles = articles.slice(
-//         startIndex,
-//         startIndex + ARTICLES_PER_PAGE
-//     );
-//     const pageCount = Math.ceil(articles.length / ARTICLES_PER_PAGE);
-//     return (
-//         <main>
-//             <div className="container posts" id="posts">
-//                 <h1 className="heading-large mt-5">Page {currentPage}</h1>
-//                 <div className="row post-list">
-//                     {paginatedArticles.map(
-//                         (post: ArticleItem, index: number | undefined) => (
-//                             <PostCard
-//                                 post={post}
-//                                 style="full"
-//                                 index={index}
-//                                 key={index}
-//                             />
-//                         )
-//                     )}
-//                 </div>
-//                 <div className="row">
-//                     <div className="d-flex pagination justify-content-center">
-//                         {Array.from({length: pageCount}, (_, i) => (
-//                             <a
-//                                 className="mb-5 mx-2"
-//                                 key={i}
-//                                 href={`/page/${i + 1}`}>
-//                                 <button className="px-3 py-2">{i + 1}</button>
-//                             </a>
-//                         ))}
-//                     </div>
-//                 </div>
-//             </div>
-//         </main>
-//     );
-// }
+export default async function BlogPage({params}: {params: {page: string}}) {
+    const currentPage = parseInt(params.page, 10) || 1;
+    const posts: PostItem[] = await getSortedPosts();
+    const authorData: AuthorItem[] = await getUsers();
 
-export default async function BlogPage() {
-    return null;
+    const startIndex = (currentPage - 1) * ARTICLES_PER_PAGE;
+    const paginatedArticles = posts.slice(
+        startIndex,
+        startIndex + ARTICLES_PER_PAGE
+    );
+    const pageCount = Math.ceil(posts.length / ARTICLES_PER_PAGE);
+    return (
+        <main>
+            <div className="container posts" id="posts">
+                <h1 className="heading-large mt-5">Page {currentPage}</h1>
+                <div className="row post-list">
+                    {paginatedArticles.map(
+                        (post: PostItem, index: number | undefined) => (
+                            <PostCard
+                                post={post}
+                                style="full"
+                                index={index}
+                                key={index}
+                                authorData={
+                                    authorData.find(
+                                        author => author.email === post.email
+                                    ) as AuthorItem
+                                }
+                            />
+                        )
+                    )}
+                </div>
+                <div className="row">
+                    <div className="d-flex pagination justify-content-center">
+                        {Array.from({length: pageCount}, (_, i) => (
+                            <a
+                                className="mb-5 mx-2"
+                                key={i}
+                                href={`/page/${i + 1}`}>
+                                <button className="px-3 py-2">{i + 1}</button>
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 }
