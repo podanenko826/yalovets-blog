ad19339f4465e9c79ab67cadada09857c2407935
Denis Podanenko <podanenko826@icloud.com>
2025-07-12 22:54:09 +0300

    Remove console logs

--- src/components/PostCard/PostList.tsx
+++ src/components/PostCard/PostList.tsx
@@ -74,13 +74,8 @@ const PostList: React.FC<PostListProps> = ({ displayMode, style, limit, indexInc
         }, 1500)
     };
 
-    console.log('posts: ', posts);
-    console.log('authos: ', authors);
-    
-
     if (posts.length === 0 || authors.length === 0) return;
     
-
     return (
         <>
             {/* Render dynamically fetched posts */}
