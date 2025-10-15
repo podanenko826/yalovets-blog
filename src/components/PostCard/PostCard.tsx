38c1513c5b16cd938e0e7e76dd82265e09e5d6a5
Denis Podanenko <podanenko826@icloud.com>
2025-10-15 15:06:52 +0200

    Add views count to expanded PostCard

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -448,7 +448,7 @@ const PostCard = ({ post, previewData, authorData, style, index, setValue, setPo
                                     </Link>
 
                                     <p className={`${styles.profile_info__text} align-content-center m-0`} id="col-heading-1">
-                                        {moment(post.date).format('D MMM')} • {post.readTime?.toString()} min read
+                                        {moment(post.date).format('D MMM')} • {post.readTime?.toString()} min read • {post.viewsCount} views
                                     </p>
                                 </div>
                             </div>
